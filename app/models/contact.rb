f351b79b61fcfcdc88785fe3def52cc20c6db6a9
tommy3wood <thomaswcurran1014@gmail.com>
2020-01-20 12:00:56 -0600

    model methods and partials

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,13 @@
 class Contact < ApplicationRecord
+  def friendly_updated_at
+    updated_at.strftime("%b %d, %Y")
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def country_code
+    "+81 #{phone_number}"
+  end
 end
